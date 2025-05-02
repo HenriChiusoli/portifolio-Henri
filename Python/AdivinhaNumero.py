c8451ea413cafcdf74803513ae5f64cb2819282a
HenriChiusoli <henrichiusoli2003@gmail.com>
2025-05-02 19:54:59 -0300

    atualizando codigo

--- Python/AdivinhaNumero.py
+++ Python/AdivinhaNumero.py
@@ -1,4 +1,22 @@
 import random
 
-print("Olá, bem-vindo ao jogo de adivinhação!")
-print("Estou pensando em um número de 1 a 100. Tente adivinhar!")
+def adivinhe_o_numero():
+    numero_secreto = random.randint(1, 100)
+    tentativas = 0
+
+    print("Bem-vindo ao jogo de adivinhação!")
+    print("Tente adivinhar o número entre 1 e 100.")
+
+    while True:
+        tentativa = int(input("Digite seu palpite: "))
+        tentativas += 1
+
+        if tentativa < numero_secreto:
+            print("Muito baixo! Tente novamente.")
+        elif tentativa > numero_secreto:
+            print("Muito alto! Tente novamente.")
+        else:
+            print(f"Parabéns! Você acertou em {tentativas} tentativas.")
+            break
+
+adivinhe_o_numero()
